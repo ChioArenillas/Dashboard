722f4873ca2d4010116d720af35abcd537d7d827
Rocio Arenillas (Chio) <arenillasr@gmail.com>
2026-03-17 13:45:27 +0100

    new

--- index.js
+++ index.js
@@ -1,36 +1,29 @@
 
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
-    .then(resp => resp.json())
-    .then(data => {
-        document.body.style.backgroundImage = `url(${data.urls.regular})`
-        document.getElementById("autor").innerHTML = `Photo By: ${data.user.name}`
-    })
-    .catch(err => {
-        console.log(err)
-        document.body.style.backgroundImage = `url("https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NzM3NDAwNTJ8&ixlib=rb-4.1.0&q=80&w=1080")`
-        document.getElementById("autor").innerHTML = `By: Kalem Emsley`
+try {
+    const res = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+    const data = await res.json()
+    document.body.style.backgroundImage = `url(${data.urls.regular})`
+    document.getElementById("autor").innerHTML = `Photo By: ${data.user.name}`
+} catch (err) {
+    document.body.style.backgroundImage = `url("https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NzM3NDAwNTJ8&ixlib=rb-4.1.0&q=80&w=1080")`
+    document.getElementById("autor").innerHTML = `By: Kalem Emsley`
+}
 
-    })
-
-fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
-    .then(resp => {
-        if (!resp.ok) {
-            throw Error("Something went wrong")
-        }
-        return resp.json()
-    })
-    .then(data => {
-        document.getElementById("icon").innerHTML = `<img src="${data.image.small}" />`
-        document.getElementById("name").innerHTML = data.name
-        document.getElementById("price").innerHTML = `
+try {
+    const resp = await fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
+    if (!resp.ok) {
+        throw Error("Something went wrong")
+    }
+    const data = await resp.json()
+    document.getElementById("icon").innerHTML = `<img src="${data.image.small}" />`
+    document.getElementById("name").innerHTML = data.name
+    document.getElementById("price").innerHTML = `
             <div>🎯: ${data.market_data.current_price.eur} €</div>
             <div>📈: ${data.market_data.high_24h.eur} €</div>
             <div>📉: ${data.market_data.low_24h.eur} €</div>`
-    })
-    .catch(err => {
-        console.log(err)
-
-    })
+} catch (err) {
+    console.error(err)
+}
 
 function updateClock() {
     const now = new Date()
@@ -51,25 +44,25 @@ function updateClock() {
 setInterval(updateClock, 1000)
 
 
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(async (pos) => {
-        const lat = pos.coords.latitude
-        const lon = pos.coords.longitude
-        const weatherresp = await fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric`)
-        const weatherdata = await weatherresp.json()
-        console.log(weatherdata)
-        document.getElementById("weather").innerHTML = `
-        <div class="weatherTop">
-            <img src="http://openweathermap.org/img/wn/${weatherdata.weather[0].icon}@2x.png"/>
-            ${Math.round(weatherdata.main.temp)}º
-        </div>
-        <div>${weatherdata.weather[0].main}</div>
-        <div>${weatherdata.name}, ${weatherdata.sys.country}</div>
-        `
 
-    }, (error) => {
-        console.error("Error al obtener la ubicación: ", error.message);
-    });
-} else {
-    console.log("Tu navegador no soporta geolocalización.");
-} 
+navigator.geolocation.getCurrentPosition(async pos => {
+    try {
+    const lat = pos.coords.latitude
+    const lon = pos.coords.longitude
+    const res = await fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric`)
+    if (!res.ok) {
+        throw Error("Weather data not available")
+    }
+    const data = await res.json()
+    document.getElementById("weather").innerHTML = `
+        <div class="weatherTop">
+            <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"/>
+            ${Math.round(data.main.temp)}º
+        </div>
+        <div>${data.weather[0].main}</div>
+        <div>${data.name}, ${data.sys.country}</div>
+        `
+    } catch (err){
+        console.error(err)
+    }
+})
